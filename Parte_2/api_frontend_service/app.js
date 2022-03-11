06634e96cd17b2b59920bcf61db8120b66111ec4
Juan Urdaneta <67289885+Juandurdaneta@users.noreply.github.com>
2022-03-10 21:25:46 -0400

    Update en api_frontend

--- Parte_2/api_frontend_service/app.js
+++ Parte_2/api_frontend_service/app.js
@@ -16,46 +16,187 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 // config bd
 mongoose.connect("mongodb://localhost:27017/users", { useUnifiedTopology: true, useNewUrlParser: true });
 
-// // login 
-// app.post('/login', (req, res)=>{
+// login 
+app.post('/login', (req, res)=>{
 
-//     const body = {
-//         username: req.body.username,
-//         password: req.body.password
-//     }
-
-//     fetch('http://127.0.0.1:3000/login',{
-//         method: 'post',
-//         body: JSON.stringify(body),
-//         headers: {'Content-Type': 'application/json'}
-//     })
+    const body = {
+        username: req.body.username,
+        password: req.body.password
+    }
 
 
-//     .then(res => res.json())
-//     .then(json => res.send(json));
 
-// })
-
-// // register
-
-// app.post('/register', (req, res)=>{
-
-//     const body = {
-//         username: req.body.username,
-//         password: req.body.password
-//     }
-
-//     fetch('http://127.0.0.1:3000/register',{
-//         method: 'post',
-//         body: JSON.stringify(body),
-//         headers: {'Content-Type': 'application/json'}
-//     })
-
-//     .then(res => res.json())
-//     .then(json => res.send(json));
+    fetch('http://127.0.0.1:3000/login',{
+        method: 'post',
+        body: JSON.stringify(body),
+        headers: {'Content-Type': 'application/json'}
+    })
 
 
-// })
+    .then(res => res.json())
+    .then(json => {
+        if(json.status === 200){
+            jwt.sign(body, 'secretKey', (err, token) =>{
+                if(!err){
+                    res.send({
+                        status: 200,
+                        mensaje: "Inicio de sesion exitoso!.",
+                        token: token
+                    })
+                } else {
+                    res.send({
+                        status: 100,
+                        mensaje: "Inicio de sesion fallido. Vuelve a intentarlo"
+                    })
+                }
+            })
+        } else {
+            res.send(json)
+        }
+    });
+
+})
+
+// register
+
+app.post('/register', (req, res)=>{
+
+    const body = {
+        username: req.body.username,
+        password: req.body.password
+    }
+
+    fetch('http://127.0.0.1:3000/register',{
+        method: 'post',
+        body: JSON.stringify(body),
+        headers: {'Content-Type': 'application/json'}
+    })
+
+    .then(res => res.json())
+    .then(json => res.send(json));
+
+
+})
+
+// obtener peliculas
+
+app.get('/movies', verifyToken, (req, res)=>{
+    console.log(req.token)
+    jwt.verify(req.token, 'secretKey', (err, authData)=>{
+        if(err){
+            res.sendStatus(403);
+            
+        } else{
+            fetch('http://127.0.0.1:4200/movies', {
+                method: 'get'
+            })
+            .then(res => res.json())
+            .then(json => res.send(json))
+        }
+    })
+})
+
+// obtener una pelicula en especifico
+
+app.get('/movies/:movieId', verifyToken, (req, res)=>{
+
+    jwt.verify(req.token, 'secretKey', (err, authData)=>{
+        if(err){
+            res.sendStatus(403);
+            
+        } else{
+
+            fetch(`http://127.0.0.1:4200/movies/${req.params.movieId}`, {
+                method: 'get'
+            })
+            .then(res => res.json())
+            .then(json => res.send(json))
+        }
+    })
+})
+
+// agregar pelicula
+
+app.post('/movies', verifyToken, (req, res)=>{
+
+    const body = req.body
+
+    jwt.verify(req.token, 'secretKey', (err, authData)=>{
+        if(err){
+            res.sendStatus(403);
+            
+        } else{
+            fetch('http://127.0.0.1:4200/movies',{
+                method: 'post',
+                body: JSON.stringify(body),
+                headers: {'Content-Type': 'application/json'}
+            })
+            .then(res => res.json())
+            .then(json => res.send(json))
+        }
+    })
+})
+
+// modificar pelicula
+
+app.put('/movies/:movieId', verifyToken, (req, res)=>{
+
+    const body = req.body
+
+    jwt.verify(req.token, 'secretKey', (err, authData)=>{
+        if(err){
+            res.sendStatus(403);
+            
+        } else{
+            fetch(`http://127.0.0.1:4200/movies/${req.params.movieId}`,{
+                method: 'put',
+                body: JSON.stringify(body),
+                headers: {'Content-Type': 'application/json'}
+            })
+            .then(res => res.json())
+            .then(json => res.send(json))
+        }
+    })
+})
+
+// eliminar pelicula
+app.delete('/movies/:movieId', verifyToken, (req, res)=>{
+
+    const body = req.body
+
+    jwt.verify(req.token, 'secretKey', (err, authData)=>{
+        if(err){
+            res.sendStatus(403);
+            
+        } else{
+            fetch(`http://127.0.0.1:4200/movies/${req.params.movieId}`,{
+                method: 'delete'
+            })
+            .then(res => res.json())
+            .then(json => res.send(json))
+        }
+    })
+})
+// verificando token
+
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined') {
+      // Split at the space
+      const bearer = bearerHeader.split(' ');
+      // Get token from array
+      const bearerToken = bearer[1];
+      // Set the token
+      req.token = bearerToken;
+      // Next middleware
+      next();
+    } else {
+      // Forbidden
+      res.sendStatus(403);
+    }
+}  
 
 
 app.listen(port, ()=>{
